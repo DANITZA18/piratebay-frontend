caa162e8e8005820ceebe2bbd56b8d0b906c5dd0
danitza18 <danitzamarlenechurasocujayo@gmail.com>
2020-07-04 23:16:08 -0400

    subiendo segunda parte del archivo javascript de almacenes

--- js/almacenes.js
+++ js/almacenes.js
@@ -16,3 +16,42 @@ $(document).ready(function () {
     });
 
 });
+function cargaAlmacenes()
+{
+    $.ajax({
+        headers:{'Authorization':'bearer '+localStorage.piratebay_token},
+        type: "GET",
+        url: url_base+"/api/v1/warehouse",
+        contentType: 'application/json',
+        success: function (response) {
+            let vacio = `SIN REGISTROS`;
+            if(response.length > 0)
+            {
+                let almacen = ``;
+                $.each(response,function(i, item){
+                    almacen += `
+                            <div class="col-md-4 contenedor_almacen">
+                                <div class="almacen" 
+                                    data-url="${url_base}/api/v1/products/warehouse?id=${item.warehouse_id}&texto=" 
+                                    data-id="${item.warehouse_id}" 
+                                    data-nom="${item.warehouse_name}"
+                                    data-address="${item.warehouse_address}"
+                                    >
+                                    <div class="imagen"><i class="fa fa-building fa-8x"></i></div>
+                                    <div class="nombre">${item.warehouse_name}</div>
+                                </div>
+                            </div>
+                            `;
+                });
+                $('#contenedor').html(almacen)
+            }
+            else{
+                $('#contenedor').html(vacio)
+            }
+        },
+        error: function(){
+            usarRefresh();
+            cargaUsuarios();
+        }
+    });
+}
